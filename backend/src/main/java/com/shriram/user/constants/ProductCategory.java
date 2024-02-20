18426b83375472602a76ce8c3d55903d9fc213b6
yyashingle <yyash.ingle@gmail.com>
2024-02-20 19:34:25 +0530

    Product entity code

---
+++ backend/src/main/java/com/shriram/user/constants/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.shriram.user.constants;
+
+public enum ProductCategory {
+
+	MALE,
+	FEMALE
+}
---
+++ backend/src/main/java/com/shriram/user/constants/ProductSize.java
@@ -0,0 +1,13 @@
+package com.shriram.user.constants;
+
+public enum ProductSize {
+
+	 	XS,
+	    S,
+	    M,
+	    L,
+	    XL,
+	    XXL,
+	    XXXL,
+	    XXXXL
+}
